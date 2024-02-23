6e20443ba95c05b087f0450de65bbf71e0c9b434
Luc Belliveau <luc.belliveau@phac-aspc.gc.ca>
2024-02-23 14:34:39 -0400

    chore: added unit tests and workflow (#31)
    Added basic unit test configuration
    Added lint and test workflow action
    
    Created time travel component and tests

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,12 +1,20 @@
 import { useTranslations } from "next-intl";
 import AppWrapper from "../_components/AppWrapper";
+import TimeTravel from "../_components/TimeTravel";
 
 export default function Index() {
   const t = useTranslations("App");
+  const timeTravelMsg = useTranslations("TimeTravel");
 
   return (
     <AppWrapper title={t("title")}>
-      <h1> {t("title")}</h1>
+      <TimeTravel
+        messages={{
+          chooseDate: timeTravelMsg("chooseDate"),
+          currentDate: timeTravelMsg("currentDate"),
+          travelText: timeTravelMsg("travelText"),
+        }}
+      />
     </AppWrapper>
   );
 }
