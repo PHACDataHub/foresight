16cd4d6284a911e2f23be9a15960abca5f966eb1
Nghia Doan <nghia.doan@phac-aspc.gc.ca>
2024-02-27 15:43:19 +0000

    Renaming

---
+++ backend/pipeline/llm-who-dons.py
@@ -0,0 +1,209 @@
+import csv
+import json
+import sys
+
+from langchain.chains.llm import LLMChain
+from langchain.chains.summarize import load_summarize_chain
+from langchain.output_parsers.list import NumberedListOutputParser
+from langchain.prompts.prompt import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+
+TOPIC_LIST = [
+    'Outbreaks of known infectious diseases',
+    'Emerging infectious diseases or novel pathogens',
+    'Hurricanes, earthquakes, floods, wildfires, and their health impacts',
+    'Effects on health infrastructure and services during disasters',
+    'Air pollution levels and associated health risks',
+    'Water contamination issues and their health implications',
+    'Chemical spills or industrial accidents affecting public health',
+    'Health implications of accidents in industrial settings',
+    'Potential biological attacks or bioterrorism threats',
+    'Reports on suspicious disease-related incidents', 
+    'Reports on suspicious drug-related incidents', 
+    'Foodborne illness outbreaks and recalls',
+    'Waterborne diseases and contamination alerts',
+    'Incidents involving radiation exposure and their health consequences',
+    'Extreme weather events and health advisories',
+    'Health implications of changing climate patterns',
+    'Outbreaks linked to vaccine-preventable diseases',
+    'Controversies or developments in vaccination policies',
+    'Security breaches or cyberattacks on healthcare systems',
+    'Risks to patient data and healthcare services',
+    'Evaluations of healthcare system readiness during emergencies',
+    'Reports on hospital capacity during crises or emergencies',
+    'Drug recalls, counterfeit drugs, and safety concerns',
+    'Issues with medical device safety, recalls, and their impact on health',
+    'Unusual health patterns',
+    'Emerging pathogens',
+    'Abnormal environmental indicators',
+    'Unforeseen health outcomes',
+    'Anomalous disease clusters',
+    'Unrecognized health risks',
+    'Atypical health incidents'
+]
+
+CLASSIFICATION_TEMPLATE = """
+Return ONLY the topic classifications listed below
+TOPICS:{topics}
+
+that best matches the article delimited by triple backquotes (```)
+ARTICLE:```{article}```
+
+If there is no such topic classification, then return No match as result.
+RESULT:{format_instructions}"""
+
+QA_TEMPLATE = """Provide a short and concise answer for each of the following questions using ONLY information found in the article delimited by triple backquotes (```).
+Do not explain.
+QUESTIONS:{questions}
+
+ARTICLE:```{article}```
+
+ANSWERS:{format_instructions}:"""
+
+QUESTION_LIST = [
+    'What disease is it or is it an unknown disease? ',
+    'What is the possible source of this disease?',
+    'How does this disease spread?',
+    'What are the countries effected by the disease?',
+    'When did the disease start?',
+    'Is the disease still spreading or did it stop?',
+]    
+
+QUESTION_LIST = [
+    'What disease is it or is it an unknown disease? ',
+    'What is the possible source of this disease?',
+    'How does this disease spread?',
+    'What are the countries effected by the disease?',
+    'When did the disease start?',
+    'Is the disease still spreading or did it stop?',
+]
+
+MAPPING_TEMPLATE = """Return the ISO 3166 Alpha 2 two-letter country code for each of the country names found in the content .
+Do not explain.
+CONTENT:{content}
+
+CODE:{format_instructions}:"""
+
+
+def increase_count(count, character):
+    count += 1
+    print(character, end="", flush=True)
+    return count
+
+
+def create_out_file_name(in_file_name):
+    in_file_splits = in_file_name.split('/')
+    out_file_name = '/'.join(in_file_splits[:-1]) + '/' + 'processed-' + in_file_splits[-1]
+    return out_file_name
+
+
+def load_country_codes(file_name):
+    country_dict = dict()
+    with open(file_name, 'rt') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter='\t')
+        for row in csv_reader:
+            country_dict[row["code"]] = row['name']
+    print(f"Read {len(country_dict)} countries.\n")
+    return country_dict
+
+
+class Pipeline(object):
+    
+    def __init__(self, device) -> None:
+        self.llm = Ollama(model="mistral:instruct")
+        self.embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2', model_kwargs = {'device': device})
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=384, chunk_overlap=0)
+        self.summarize_chain = load_summarize_chain(llm=self.llm, chain_type='map_reduce') 
+        
+        self.output_parser = NumberedListOutputParser()
+        format_instructions = self.output_parser.get_format_instructions()
+
+        classify_prompt = PromptTemplate(
+            input_variables=["topics", "article"], 
+            partial_variables={"format_instructions": format_instructions}, 
+            template=CLASSIFICATION_TEMPLATE)
+        self.classify_chain = LLMChain(llm=self.llm, prompt=classify_prompt)
+
+        qa_prompt = PromptTemplate(
+            input_variables=["questions", "article"], 
+            partial_variables={"format_instructions": format_instructions}, 
+            template=QA_TEMPLATE)
+        self.qa_chain = LLMChain(llm=self.llm, prompt=qa_prompt)
+
+        mapping_prompt = PromptTemplate(
+            input_variables=["content"], 
+            partial_variables={"format_instructions": format_instructions}, 
+            template=MAPPING_TEMPLATE)
+        self.mapping_chain = LLMChain(llm=self.llm, prompt=mapping_prompt)
+ 
+    def create_embeddings(self, document, prop_content=['title', 'content'], prop_chunks='chunks', prop_embeddings='embeddings'):
+        content = '\n\n'.join([document[e] for e in prop_content if e in document])
+        document[prop_chunks] = self.text_splitter.create_documents([content])
+        document[prop_embeddings] = self.embeddings.embed_documents([c.page_content for c in document[prop_chunks]])
+        return document
+        
+    def create_summary(self, document, prop_summary='summary', prop_content='chunks', prop_output='output_text'):
+        document[prop_summary] = self.summarize_chain.invoke(document[prop_content])[prop_output]
+        return document
+    
+    def chunks_to_texts(self, document, prop_chunks='chunks'):
+        document[prop_chunks] = [chunk.page_content for chunk in document[prop_chunks]]
+        return document
+
+    def classify(self, document, prop_topics='topics', param_topics='topics', param_content='article', prop_content='summary', prop_output='text'):
+        output = self.classify_chain.invoke({param_topics: TOPIC_LIST, param_content: document[prop_content]})[prop_output]
+        document[prop_topics] = []
+        for predicted_topic in self.output_parser.parse(output):
+            for topic in TOPIC_LIST:
+                if topic in predicted_topic:
+                    document[prop_topics].append(topic)
+                    break
+        return document
+
+    def qa(self, document, prop_answers='answers', param_questions='questions', param_content='article', prop_content=['title', 'summary'], prop_output='text'): 
+        content = '\n\n'.join([document[e] for e in prop_content if e in document])
+        document[prop_answers] = []
+        for i, question in enumerate(QUESTION_LIST):
+            output = self.qa_chain.invoke({param_questions: [question], param_content: content})[prop_output]
+            document[prop_answers].append([question, [e for e in self.output_parser.parse(output) if e != '-' ]])
+        return document
+
+    def mapping(self, content, param_content='content', prop_output='text'):
+        output = self.mapping_chain.invoke({param_content: content})[prop_output]
+        return [e for e in self.output_parser.parse(output) if e != '-' ]
+
+    
+if __name__ == '__main__':
+    in_file_name, country_file_name = sys.argv[1], sys.argv[2]
+    country_dict = load_country_codes(country_file_name)
+    
+    with open(in_file_name, 'rt') as in_file:
+        documents = [json.loads(line.strip()) for line in in_file.readlines()]
+    print(f"Read {len(documents)} articles.\n")
+
+    device = sys.argv[3] if len(sys.argv) > 3 else 'cuda'
+    pipeline = Pipeline(device)
+
+    with open(create_out_file_name(in_file_name), 'wt') as out_file:
+        count = 0
+        for document in documents:
+            document = pipeline.create_embeddings(document)
+            document = pipeline.create_summary(document)
+            document = pipeline.classify(document)
+            document = pipeline.qa(document)
+            document['countries'] = list(set([e for e in pipeline.mapping(document['answers'][3][1]) if e in country_dict]))
+            if not document['countries']:
+                document['countries'] = list(set([e for e in pipeline.mapping(document['answers'][3][1]) if e in country_dict]))
+            document = pipeline.chunks_to_texts(document)
+            
+            count += 1
+            print(f"""[{count}] {document['url']}\n--- {document['summary']}\n--- [{len(document['topics'])}] {document['topics']}\n--- [{len(document['answers'])}] {document['answers']}\n--- [{len(document['countries'])}] {document['countries']}\n--- {[(pipeline.llm.get_num_tokens(e), len(document['embeddings'][i])) for i, e in enumerate(document['chunks'])]}\n""")
+            # count = increase_count(count, '.')
+
+            out_file.write(f"{json.dumps(document)}\n")
+
+    print(f"\nProcessed {count} articles.\n")
--- backend/pipeline/process-who-dons.py
+++ backend/pipeline/process-who-dons.py
@@ -2,15 +2,9 @@ import csv
 import json
 import sys
 
-from langchain.chains.llm import LLMChain
-from langchain.chains.summarize import load_summarize_chain
-from langchain.output_parsers.list import NumberedListOutputParser
-from langchain.prompts.prompt import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-from langchain_community.llms import Ollama
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
+from sentence_transformers import SentenceTransformer
+from transformers import pipeline, AutoTokenizer
 
 TOPIC_LIST = [
     'Outbreaks of known infectious diseases',
@@ -46,47 +40,15 @@ TOPIC_LIST = [
     'Atypical health incidents'
 ]
 
-CLASSIFICATION_TEMPLATE = """
-Return ONLY the topic classifications listed below
-TOPICS:{topics}
-
-that best matches the article delimited by triple backquotes (```)
-ARTICLE:```{article}```
-
-If there is no such topic classification, then return No match as result.
-RESULT:{format_instructions}"""
-
-QA_TEMPLATE = """Provide a short and concise answer for each of the following questions using ONLY information found in the article delimited by triple backquotes (```).
-Do not explain.
-QUESTIONS:{questions}
-
-ARTICLE:```{article}```
-
-ANSWERS:{format_instructions}:"""
 
 QUESTION_LIST = [
     'What disease is it or is it an unknown disease? ',
     'What is the possible source of this disease?',
     'How does this disease spread?',
-    'What are the countries effected by the disease?',
+    'What countries does the disease spread?',
     'When did the disease start?',
     'Is the disease still spreading or did it stop?',
 ]    
-
-QUESTION_LIST = [
-    'What disease is it or is it an unknown disease? ',
-    'What is the possible source of this disease?',
-    'How does this disease spread?',
-    'What are the countries effected by the disease?',
-    'When did the disease start?',
-    'Is the disease still spreading or did it stop?',
-]
-
-MAPPING_TEMPLATE = """Return the ISO 3166 Alpha 2 two-letter country code for each of the country names found in the content .
-Do not explain.
-CONTENT:{content}
-
-CODE:{format_instructions}:"""
 
 
 def increase_count(count, character):
@@ -106,103 +68,69 @@ def load_country_codes(file_name):
     with open(file_name, 'rt') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter='\t')
         for row in csv_reader:
-            country_dict[row["code"]] = row['name']
+            country_dict[row['name']] = row["code"]
     print(f"Read {len(country_dict)} countries.\n")
     return country_dict
 
 
-class Pipeline(object):
-    
-    def __init__(self, device) -> None:
-        self.llm = Ollama(model="mistral:instruct")
-        self.embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2', model_kwargs = {'device': device})
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=384, chunk_overlap=0)
-        self.summarize_chain = load_summarize_chain(llm=self.llm, chain_type='map_reduce') 
-        
-        self.output_parser = NumberedListOutputParser()
-        format_instructions = self.output_parser.get_format_instructions()
-
-        classify_prompt = PromptTemplate(
-            input_variables=["topics", "article"], 
-            partial_variables={"format_instructions": format_instructions}, 
-            template=CLASSIFICATION_TEMPLATE)
-        self.classify_chain = LLMChain(llm=self.llm, prompt=classify_prompt)
-
-        qa_prompt = PromptTemplate(
-            input_variables=["questions", "article"], 
-            partial_variables={"format_instructions": format_instructions}, 
-            template=QA_TEMPLATE)
-        self.qa_chain = LLMChain(llm=self.llm, prompt=qa_prompt)
-
-        mapping_prompt = PromptTemplate(
-            input_variables=["content"], 
-            partial_variables={"format_instructions": format_instructions}, 
-            template=MAPPING_TEMPLATE)
-        self.mapping_chain = LLMChain(llm=self.llm, prompt=mapping_prompt)
- 
-    def create_embeddings(self, document, prop_content=['title', 'content'], prop_chunks='chunks', prop_embeddings='embeddings'):
-        content = '\n\n'.join([document[e] for e in prop_content if e in document])
-        document[prop_chunks] = self.text_splitter.create_documents([content])
-        document[prop_embeddings] = self.embeddings.embed_documents([c.page_content for c in document[prop_chunks]])
-        return document
-        
-    def create_summary(self, document, prop_summary='summary', prop_content='chunks', prop_output='output_text'):
-        document[prop_summary] = self.summarize_chain.invoke(document[prop_content])[prop_output]
-        return document
-    
-    def chunks_to_texts(self, document, prop_chunks='chunks'):
-        document[prop_chunks] = [chunk.page_content for chunk in document[prop_chunks]]
-        return document
-
-    def classify(self, document, prop_topics='topics', param_topics='topics', param_content='article', prop_content='summary', prop_output='text'):
-        output = self.classify_chain.invoke({param_topics: TOPIC_LIST, param_content: document[prop_content]})[prop_output]
-        document[prop_topics] = []
-        for predicted_topic in self.output_parser.parse(output):
-            for topic in TOPIC_LIST:
-                if topic in predicted_topic:
-                    document[prop_topics].append(topic)
-                    break
-        return document
-
-    def qa(self, document, prop_answers='answers', param_questions='questions', param_content='article', prop_content=['title', 'summary'], prop_output='text'): 
-        content = '\n\n'.join([document[e] for e in prop_content if e in document])
-        document[prop_answers] = []
-        for i, question in enumerate(QUESTION_LIST):
-            output = self.qa_chain.invoke({param_questions: [question], param_content: content})[prop_output]
-            document[prop_answers].append([question, [e for e in self.output_parser.parse(output) if e != '-' ]])
-        return document
-
-    def mapping(self, content, param_content='content', prop_output='text'):
-        output = self.mapping_chain.invoke({param_content: content})[prop_output]
-        return [e for e in self.output_parser.parse(output) if e != '-' ]
+def extract_country(text, country_dict):
+    return [country_dict[country_name] for country_name in country_dict if country_name in text]
 
     
 if __name__ == '__main__':
     in_file_name, country_file_name = sys.argv[1], sys.argv[2]
+    device = sys.argv[3] if len(sys.argv) > 3 else 'cuda'
+
     country_dict = load_country_codes(country_file_name)
     
     with open(in_file_name, 'rt') as in_file:
         documents = [json.loads(line.strip()) for line in in_file.readlines()]
     print(f"Read {len(documents)} articles.\n")
 
-    device = sys.argv[3] if len(sys.argv) > 3 else 'cuda'
-    pipeline = Pipeline(device)
+    tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=0)
+    devices = [0, 0, 0, 0]
+    if device == 'cuda':
+        devices = [0, 1, 2, 3]
+    sentence_transformer = SentenceTransformer('sentence-transformers/all-mpnet-base-v2', device=devices[0])
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn",  device=devices[1])
+    answerer = pipeline('question-answering', model="deepset/roberta-base-squad2", tokenizer="deepset/roberta-base-squad2",  device=devices[2])
+    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli",  device=devices[3])
 
     with open(create_out_file_name(in_file_name), 'wt') as out_file:
         count = 0
         for document in documents:
-            document = pipeline.create_embeddings(document)
-            document = pipeline.create_summary(document)
-            document = pipeline.classify(document)
-            document = pipeline.qa(document)
-            document['countries'] = list(set([e for e in pipeline.mapping(document['answers'][3][1]) if e in country_dict]))
-            if not document['countries']:
-                document['countries'] = list(set([e for e in pipeline.mapping(document['answers'][3][1]) if e in country_dict]))
-            document = pipeline.chunks_to_texts(document)
+            full_text = '\n\n'.join([document[e] for e in ['title', 'content'] if e in document])
+
+            document['chunks'] = [c.page_content for c in text_splitter.create_documents([full_text])]
+            
+            summary_texts = [chunk if len(tokenizer(chunk)) < 143 else None for chunk in document['chunks']]
+            long_texts = [chunk for chunk in document['chunks'] if len(tokenizer(chunk)) > 142]
+            summarized_texts = [e['summary_text'] for e in summarizer(long_texts)]
+            j = 0
+            for i in range(0, len(document['chunks'])):
+                if summary_texts[i] is None:
+                    summary_texts[i] = summarized_texts[j]
+                    j += 1
+            
+            document['summary'] = '\n\n'.join(summary_texts)
+            if len(tokenizer(document['summary'])) > 1024:
+                document['summary'] = summarizer(document['summary'])[0]['summary_text']
+
+            result = classifier(document['summary'], TOPIC_LIST, multi_label=True)
+            document['topics'] = {topic: score for topic, score in zip(result['labels'], result['scores']) if score > 0.9}
+
+            document['answers'] = []
+            for i, question in enumerate(QUESTION_LIST):
+                result = answerer({'question': question, 'context': full_text})
+                document['answers'].append({'question': question, 'score': result['score'], 'answer': result['answer']})
+                if question == 'What countries does the disease spread?':
+                    document['countries'] = extract_country(result['answer'], country_dict)
+
+            document['embeddings'] = sentence_transformer.encode(document['chunks']).tolist()
             
             count += 1
-            print(f"""[{count}] {document['url']}\n--- {document['summary']}\n--- [{len(document['topics'])}] {document['topics']}\n--- [{len(document['answers'])}] {document['answers']}\n--- [{len(document['countries'])}] {document['countries']}\n--- {[(pipeline.llm.get_num_tokens(e), len(document['embeddings'][i])) for i, e in enumerate(document['chunks'])]}\n""")
-            # count = increase_count(count, '.')
+            print(f"[{count}] {document['url']}\n--- {document['summary']}\n--- [{len(document['topics'])}] {document['topics']}\n--- [{len(document['answers'])}] {document['answers']}\n--- {document['countries']}\n--- {document['chunks']}\n--- {[len(document['embeddings'][i]) for i, e in enumerate(document['chunks'])]}\n")
 
             out_file.write(f"{json.dumps(document)}\n")
 
