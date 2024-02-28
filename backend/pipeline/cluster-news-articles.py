4faa2b5bec87c448f9e305aabe6249f6253e4c74
Nghia Doan <nghia.dh@mac.com>
2024-02-28 09:56:38 -0800

    Initial clustering

--- backend/pipeline/cluster-news-articles.py
+++ backend/pipeline/cluster-news-articles.py
@@ -1,16 +1,18 @@
 import json
 import os
-import pickle
 import sys
 
-from umap import UMAP
-from hdbscan import HDBSCAN
 from sentence_transformers import SentenceTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 
 from bertopic import BERTopic
 from bertopic.representation import KeyBERTInspired
 from bertopic.vectorizers import ClassTfidfTransformer
+
+from langchain.chains.llm import LLMChain
+from langchain_community.llms import Ollama
+from langchain.output_parsers.list import NumberedListOutputParser
+from langchain.prompts import PromptTemplate
 
 
 def increase_count(count, character):
@@ -20,16 +22,26 @@ def increase_count(count, character):
 
 
 if __name__ == '__main__':
-    path, country_file_name = sys.argv[1], sys.argv[2]
-    device = sys.argv[3] if len(sys.argv) > 3 else 'cuda'
-    
+    path, country_file_name, single_date = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    device = sys.argv[4] if len(sys.argv) > 4 else 'cuda'
+    if device == 'cuda':
+        from cuml.cluster import HDBSCAN
+        from cuml.manifold import UMAP
+        from cuml.preprocessing import normalize
+    else:
+        from umap import UMAP
+        from hdbscan import HDBSCAN
+
     doc_dict = dict()
     for file in sorted(os.listdir(path)):
         file_name = os.path.join(path, file)
-        if not os.path.isfile(file_name) \
-            or not file.startswith('processed-') \
-            or not file.endswith('-news-articles.jsonl'):
+        if not single_date in file_name:
             continue
+        # if not os.path.isfile(file_name) \
+        #     or not file.startswith('processed-') \
+        #     or not file.endswith('-news-articles.jsonl'):
+        #     continue
         
         pub_date = file[10:20]
         if file.endswith('.jsonl'):
@@ -67,32 +79,94 @@ if __name__ == '__main__':
 
     # All steps together
     topic_model = BERTopic(
-        embedding_model=embedding_model,          # Step 1 - Extract embeddings
-        umap_model=umap_model,                    # Step 2 - Reduce dimensionality
-        hdbscan_model=hdbscan_model,              # Step 3 - Cluster reduced embeddings
-        vectorizer_model=vectorizer_model,        # Step 4 - Tokenize topics
-        ctfidf_model=ctfidf_model,                # Step 5 - Extract topic words
-        representation_model=representation_model # Step 6 - (Optional) Fine-tune topic representations
+        embedding_model=embedding_model,            # Step 1 - Extract embeddings
+        umap_model=umap_model,                      # Step 2 - Reduce dimensionality
+        hdbscan_model=hdbscan_model,                # Step 3 - Cluster reduced embeddings
+        vectorizer_model=vectorizer_model,          # Step 4 - Tokenize topics
+        ctfidf_model=ctfidf_model,                  # Step 5 - Extract topic words
+        representation_model=representation_model,  # Step 6 - (Optional) Fine-tune topic representations
+        calculate_probabilities=True,
     )
+    
+    # System prompt describes information given to all conversations
+    LABELING_PROMPT_TEMPLATE = """
+    You are a helpful, respectful and honest assistant for labeling topics.
+
+    I have a topic that contains the following documents delimited by triple backquotes (```). 
+    ```{documents}```
+    
+    The topic is described by the following keywords delimited by triple backquotes (```):
+    ```{keywords}```
+
+    Create a concise label of this topic, which should not exceed 32 characters.
+    If there are more than one possible labels, return the shortest one and nothing more.
+    
+    {format_instructions}
+    """
+    
+    llm = Ollama(model="mistral:instruct")
+    output_parser = NumberedListOutputParser()
+    format_instructions = output_parser.get_format_instructions()
+
+    labeling_prompt = PromptTemplate(
+        input_variables=["documents", "keywords"], 
+        partial_variables={"format_instructions": format_instructions}, 
+        template=LABELING_PROMPT_TEMPLATE)
+    labeling_llm_chain = LLMChain(llm=llm, prompt=labeling_prompt)
+
+    SUMMARY_PROMPT_TEMPLATE = """Write a concise summary for all following documents delimited by triple backquotes (```).
+    ```{documents}```
+
+    {format_instructions}"""
+    summary_prompt = PromptTemplate(
+        input_variables=["documents"], 
+        partial_variables={"format_instructions": format_instructions},
+        template=SUMMARY_PROMPT_TEMPLATE)
+    summary_llm_chain = LLMChain(llm=llm, prompt=summary_prompt)
 
     for pub_date in sorted(doc_dict.keys())[0:1]:
         texts = ['\n\n'.join([document[prop] for prop in ['title', 'content']]) for document in doc_dict[pub_date]]
         embeddings = embedding_model.encode(texts, show_progress_bar=True)
-        topics, probs = topic_model.fit_transform(texts, embeddings)
         
+        if device == 'cuda':
+            embeddings = normalize(embeddings)
+            
+        topics, probs = topic_model.fit_transform(texts, embeddings)
         print(topic_model.get_topic_info())
         
-        viz_words = topic_model.visualize_barchart(top_n_topics=20, custom_labels=True)
-        viz_words.show()
-
+        # Reduce outliers
+        new_topics = topic_model.reduce_outliers(texts, topics, probabilities=probs, strategy="probabilities")
+        new_topics = topic_model.reduce_outliers(texts, topics, strategy="embeddings", embeddings=embeddings)
+        topic_model.update_topics(texts, topics=new_topics)
+        print(topic_model.get_topic_info())
+        
+        label_dict, summary_dict = dict(), dict()
+        for i in range(-1, len(topic_model.get_topic_info())-1):
+            if i == -1:
+                label_dict[i] = 'Outlier Topic'
+                summary_dict[i] = ''
+                
+            else:
+                keywords = topic_model.topic_labels_[i].split('_')[1:]
+                representative_docs = topic_model.representative_docs_[i]
+                output = labeling_llm_chain.invoke({'documents': representative_docs, 'keywords': keywords})['text']
+                label_dict[i] = output_parser.parse(output)[0]
+                
+                output = summary_llm_chain.invoke({'documents': representative_docs})['text']
+                summary_dict[i] = sorted(output_parser.parse(output), reverse=True)[0]
+                
+                print(f"[{i}] {label_dict[i]} --- {summary_dict[i]}")
+        
+        topic_model.set_topic_labels(label_dict)
+        print(topic_model.get_topic_info())
+        
         viz_hie_arch = topic_model.visualize_hierarchy(custom_labels=True)
-        viz_hie_arch.show()
+        viz_hie_arch.write_html("viz/" + pub_date + '-hie.html')
 
         # Run the visualization with the original embeddings
         topic_model.visualize_documents(texts, embeddings=embeddings)
 
         # Reduce dimensionality of embeddings, this step is optional but much faster to perform iteratively:
         reduced_embeddings = UMAP(n_neighbors=10, n_components=2, min_dist=0.0, metric='cosine').fit_transform(embeddings)
-        viz_docs = topic_model.visualize_documents(texts, reduced_embeddings=reduced_embeddings, width=2880, height=1620, custom_labels=True)
-        viz_docs.show()
-        
+        viz_docs = topic_model.visualize_documents(texts, reduced_embeddings=reduced_embeddings, width=1024, height=768, custom_labels=True)
+        viz_docs.write_html("viz/" + pub_date + '-cls.html')
