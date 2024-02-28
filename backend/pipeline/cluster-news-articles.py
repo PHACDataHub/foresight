c950fa6f4640e5d679a46db32bb4a270bf20b458
Nghia Doan <nghia.dh@mac.com>
2024-02-28 15:53:17 -0800

    Multiple days

--- backend/pipeline/cluster-news-articles.py
+++ backend/pipeline/cluster-news-articles.py
@@ -25,103 +25,99 @@ def increase_count(count, character):
 
 
 if __name__ == '__main__':
-    path, country_file_name, single_date = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    device = sys.argv[4] if len(sys.argv) > 4 else 'cuda'
+    path, country_file_name = sys.argv[1], sys.argv[2], 
+    device = sys.argv[3] if len(sys.argv) > 3 else 'cuda'
 
     doc_dict = dict()
     for file in sorted(os.listdir(path)):
         file_name = os.path.join(path, file)
-        if not single_date in file_name:
+        if not os.path.isfile(file_name) \
+            or not file.startswith('processed-') \
+            or not file.endswith('-news-articles.jsonl') \
+            or not file.endswith('.jsonl'):
             continue
-        # if not os.path.isfile(file_name) \
-        #     or not file.startswith('processed-') \
-        #     or not file.endswith('-news-articles.jsonl'):
-        #     continue
         
-        pub_date = file[10:20]
-        if file.endswith('.jsonl'):
-            count = 0
-            documents = []
-            with open(file_name, 'rt') as in_file:
-                for line in in_file.readlines():
-                    document = json.loads(line.strip())
-                    documents.append(document)
-                    count = increase_count(count, '.')
-            doc_dict[pub_date] = documents
-            print(f"\n[{pub_date}] Read {count} articles.\n")
-
-    # System prompt describes information given to all conversations
-    LABELING_PROMPT_TEMPLATE = """
-    You are a helpful, respectful and honest assistant for labeling topics.
-
-    I have a topic that contains the following documents delimited by triple backquotes (```). 
-    ```{documents}```
-    
-    The topic is described by the following keywords delimited by triple backquotes (```):
-    ```{keywords}```
-
-    Create a concise label of this topic, which should not exceed 32 characters.
-    If there are more than one possible labels, return the shortest one and nothing more.
-    
-    {format_instructions}
-    """
-    
-    llm = Ollama(model="mistral:instruct")
-    output_parser = NumberedListOutputParser()
-    format_instructions = output_parser.get_format_instructions()
-
-    labeling_prompt = PromptTemplate(
-        input_variables=["documents", "keywords"], 
-        partial_variables={"format_instructions": format_instructions}, 
-        template=LABELING_PROMPT_TEMPLATE)
-    labeling_llm_chain = LLMChain(llm=llm, prompt=labeling_prompt)
-
-    SUMMARY_PROMPT_TEMPLATE = """Write a concise summary for all following documents delimited by triple backquotes (```).
-    ```{documents}```
-
-    {format_instructions}"""
-    summary_prompt = PromptTemplate(
-        input_variables=["documents"], 
-        partial_variables={"format_instructions": format_instructions},
-        template=SUMMARY_PROMPT_TEMPLATE)
-    summary_llm_chain = LLMChain(llm=llm, prompt=summary_prompt)
-
-    # Step 1 - Extract embeddings
-    # device_id = 'mps' if device == 'mps' else worker_id
-    # sentence_transformer = SentenceTransformer('sentence-transformers/all-mpnet-base-v2', device=device_id)
-    embedding_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
-
-    # Step 2 - Reduce dimensionality
-    umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine')
-
-    # Step 3 - Cluster reduced embeddings
-    hdbscan_model = HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
-
-    # Step 4 - Tokenize topics
-    vectorizer_model = CountVectorizer(stop_words="english")
-
-    # Step 5 - Create topic representation
-    ctfidf_model = ClassTfidfTransformer()
-
-    # Step 6 - (Optional) Fine-tune topic representations with 
-    # a `bertopic.representation` model
-    representation_model = KeyBERTInspired()
-
-    # All steps together
-    topic_model = BERTopic(
-        embedding_model=embedding_model,            # Step 1 - Extract embeddings
-        umap_model=umap_model,                      # Step 2 - Reduce dimensionality
-        hdbscan_model=hdbscan_model,                # Step 3 - Cluster reduced embeddings
-        vectorizer_model=vectorizer_model,          # Step 4 - Tokenize topics
-        ctfidf_model=ctfidf_model,                  # Step 5 - Extract topic words
-        representation_model=representation_model,  # Step 6 - (Optional) Fine-tune topic representations
-        calculate_probabilities=True,
-    )
-       
-    for pub_date in sorted(doc_dict.keys())[0:1]:
         start_time = datetime.now()
 
+        pub_date = file[10:20]
+        count = 0
+        documents = []
+        with open(file_name, 'rt') as in_file:
+            for line in in_file.readlines():
+                document = json.loads(line.strip())
+                documents.append(document)
+                count = increase_count(count, '.')
+        doc_dict[pub_date] = documents
+        print(f"\n[{pub_date}] Read {count} articles.\n")
+
+        # System prompt describes information given to all conversations
+        LABELING_PROMPT_TEMPLATE = """
+        You are a helpful, respectful and honest assistant for labeling topics.
+
+        I have a topic that contains the following documents delimited by triple backquotes (```). 
+        ```{documents}```
+        
+        The topic is described by the following keywords delimited by triple backquotes (```):
+        ```{keywords}```
+
+        Create a concise label of this topic, which should not exceed 32 characters.
+        If there are more than one possible labels, return the shortest one and nothing more.
+        
+        {format_instructions}
+        """
+        
+        llm = Ollama(model="mistral:instruct")
+        output_parser = NumberedListOutputParser()
+        format_instructions = output_parser.get_format_instructions()
+
+        labeling_prompt = PromptTemplate(
+            input_variables=["documents", "keywords"], 
+            partial_variables={"format_instructions": format_instructions}, 
+            template=LABELING_PROMPT_TEMPLATE)
+        labeling_llm_chain = LLMChain(llm=llm, prompt=labeling_prompt)
+
+        SUMMARY_PROMPT_TEMPLATE = """Write a concise summary for all following documents delimited by triple backquotes (```).
+        ```{documents}```
+
+        {format_instructions}"""
+        summary_prompt = PromptTemplate(
+            input_variables=["documents"], 
+            partial_variables={"format_instructions": format_instructions},
+            template=SUMMARY_PROMPT_TEMPLATE)
+        summary_llm_chain = LLMChain(llm=llm, prompt=summary_prompt)
+
+        # Step 1 - Extract embeddings
+        # device_id = 'mps' if device == 'mps' else worker_id
+        # sentence_transformer = SentenceTransformer('sentence-transformers/all-mpnet-base-v2', device=device_id)
+        embedding_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+
+        # Step 2 - Reduce dimensionality
+        umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine')
+
+        # Step 3 - Cluster reduced embeddings
+        hdbscan_model = HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
+
+        # Step 4 - Tokenize topics
+        vectorizer_model = CountVectorizer(stop_words="english")
+
+        # Step 5 - Create topic representation
+        ctfidf_model = ClassTfidfTransformer()
+
+        # Step 6 - (Optional) Fine-tune topic representations with 
+        # a `bertopic.representation` model
+        representation_model = KeyBERTInspired()
+
+        # All steps together
+        topic_model = BERTopic(
+            embedding_model=embedding_model,            # Step 1 - Extract embeddings
+            umap_model=umap_model,                      # Step 2 - Reduce dimensionality
+            hdbscan_model=hdbscan_model,                # Step 3 - Cluster reduced embeddings
+            vectorizer_model=vectorizer_model,          # Step 4 - Tokenize topics
+            ctfidf_model=ctfidf_model,                  # Step 5 - Extract topic words
+            representation_model=representation_model,  # Step 6 - (Optional) Fine-tune topic representations
+            calculate_probabilities=True,
+        )
+       
         print('Compute embeddings ...')
         texts = ['\n\n'.join([document[prop] for prop in ['title', 'summary']]) for document in doc_dict[pub_date]]
         if device == 'cuda':
