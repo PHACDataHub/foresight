b1800bba9d9810ad72ce232ccb809b0ad3315368
Nghia Doan <nghia.doan@phac-aspc.gc.ca>
2024-02-29 21:13:04 +0000

    Split postprocessing

---
+++ backend/scripts/cluster-2020-news-articles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-01 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-02 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-03 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-04 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-05 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-06 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-07 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-08 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-09 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-10 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-11 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-12 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-13 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-14 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-15 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-16 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-17 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-18 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-19 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-20 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-21 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-22 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-23 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-24 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-25 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-26 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-27 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-28 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-29 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-30 cuda
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-31 cuda
