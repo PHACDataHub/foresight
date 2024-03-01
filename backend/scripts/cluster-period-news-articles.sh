b4457b9ef48bfdf0a3dafa7538f9c7c5dff32846
Nghia Doan <nghia.dh@mac.com>
2024-03-01 00:07:24 -0800

    Fix typo

--- backend/scripts/cluster-period-news-articles.sh
+++ backend/scripts/cluster-period-news-articles.sh
@@ -2,12 +2,12 @@
 
 device=$1
 
-python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2019-12-31 $device 2020-01-02
-python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-01 $device 2020-01-03
-python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-02 $device 2020-01-04
+# python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2019-12-31 $device 2020-01-02
+# python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-01 $device 2020-01-03
+# python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-02 $device 2020-01-04
 python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-03 $device 2020-01-05
 python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-04 $device 2020-01-06
 
 python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2019-12-31 $device 2020-01-06
 
-python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-12-31 $device 2020-01-29
+# python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2019-12-31 $device 2020-01-29
