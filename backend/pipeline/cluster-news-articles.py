96ab9e55456f16bb1a5a2a736aabd5c9ed403bdd
Nghia Doan <nghia.dh@mac.com>
2024-02-29 17:14:02 -0800

    Add period postprocessing

--- backend/pipeline/cluster-news-articles.py
+++ backend/pipeline/cluster-news-articles.py
@@ -239,10 +239,13 @@ def classify_text(n_workers, input_documents, device):
 if __name__ == '__main__':
     path, country_file_name, single_date = sys.argv[1], sys.argv[2], sys.argv[3]
     device = sys.argv[4] if len(sys.argv) > 4 else None
+    end_date = sys.argv[5] if len(sys.argv) > 5 else None
     
     country_dict = load_country_codes(country_file_name)
     
-    doc_dict = dict()
+    start_time = datetime.now()
+    
+    documents = []
     for file in sorted(os.listdir(path)):
         file_name = os.path.join(path, file)
         if not os.path.isfile(file_name) \
@@ -251,198 +254,206 @@ if __name__ == '__main__':
             or not file.endswith('.jsonl'):
             continue
 
-        if single_date not in file:
-            continue
-        
         pub_date = file[10:20]
-        start_time = datetime.now()
+        if end_date is None:
+            if single_date != pub_date:
+                continue
+        else:
+            if single_date > pub_date or pub_date > end_date:
+                continue
 
         count = 0
-        documents = []
-        # document_map = dict()
         with open(file_name, 'rt') as in_file:
             for line in in_file.readlines():
                 document = json.loads(line.strip())
                 documents.append(document)
                 count = increase_count(count, '.')
-                # document_map[document['title']] = document['summary']
-                
-        doc_dict[pub_date] = documents
         print(f"\n[{pub_date}] Read {count} articles.\n")
 
-        # System prompt describes information given to all conversations
-        LABELING_PROMPT_TEMPLATE = """
-        You are a helpful, respectful and honest assistant for labeling topics.
+    # System prompt describes information given to all conversations
+    LABELING_PROMPT_TEMPLATE = """
+    You are a helpful, respectful and honest assistant for labeling topics.
 
-        I have a topic that contains the following documents delimited by triple backquotes (```). 
-        ```{documents}```
-        
-        The topic is described by the following keywords delimited by triple backquotes (```):
-        ```{keywords}```
-
-        Create a concise label of this topic, which should not exceed 32 characters.
-        If there are more than one possible labels, return the shortest one and nothing more.
-        
-        {format_instructions}
-        """
-        
-        llm = Ollama(model="mistral:instruct")
-        output_parser = NumberedListOutputParser()
-        format_instructions = output_parser.get_format_instructions()
-
-        labeling_prompt = PromptTemplate(
-            input_variables=["documents", "keywords"], 
-            partial_variables={"format_instructions": format_instructions}, 
-            template=LABELING_PROMPT_TEMPLATE)
-        labeling_llm_chain = LLMChain(llm=llm, prompt=labeling_prompt)
-
-        # Step 1 - Extract embeddings
-        device_id = 'mps' if device == 'mps' else 0
-        # sentence_transformer = SentenceTransformer('sentence-transformers/all-mpnet-base-v2', device=device_id)
-        embedding_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2', device=device_id)
-
-        # Step 2 - Reduce dimensionality
-        umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine')
-
-        # Step 3 - Cluster reduced embeddings
-        hdbscan_model = HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
-
-        # Step 4 - Tokenize topics
-        vectorizer_model = CountVectorizer(stop_words="english")
-
-        # Step 5 - Create topic representation
-        ctfidf_model = ClassTfidfTransformer(reduce_frequent_words=True)
-
-        # Step 6 - (Optional) Fine-tune topic representations with 
-        # a `bertopic.representation` model
-        representation_model = KeyBERTInspired()
-
-        # All steps together
-        topic_model = BERTopic(
-            embedding_model=embedding_model,            # Step 1 - Extract embeddings
-            umap_model=umap_model,                      # Step 2 - Reduce dimensionality
-            hdbscan_model=hdbscan_model,                # Step 3 - Cluster reduced embeddings
-            vectorizer_model=vectorizer_model,          # Step 4 - Tokenize topics
-            ctfidf_model=ctfidf_model,                  # Step 5 - Extract topic words
-            representation_model=representation_model,  # Step 6 - (Optional) Fine-tune topic representations
-            calculate_probabilities=True,
-        )
-        
-        print('Compute embeddings ...')
-        texts = ['\n\n'.join([document[prop] for prop in ['title', 'content']]) for document in doc_dict[pub_date]]
-        if device == 'cuda':
-            pool = embedding_model.start_multi_process_pool(target_devices=['cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'])
-            embeddings = embedding_model.encode_multi_process(texts, pool)
-            embedding_model.stop_multi_process_pool(pool)
-        else:
-            embeddings = embedding_model.encode(texts, show_progress_bar=True)
+    I have a topic that contains the following documents delimited by triple backquotes (```). 
+    ```{documents}```
     
-        print('Start fit_transform ...')
-        topics, probs = topic_model.fit_transform(texts, embeddings)
-        topic_set = set(topics)
-        print(topic_model.get_topic_info())
-        
-        print('Reduce outliers ...')
-        new_topics = topic_model.reduce_outliers(texts, topics, probabilities=probs, strategy="probabilities")
-        new_topics = topic_model.reduce_outliers(texts, topics, strategy="embeddings", embeddings=embeddings)
-        topic_model.update_topics(texts, topics=new_topics)
-        print(topic_model.get_topic_info())
-        
-        n_workers = 4
+    The topic is described by the following keywords delimited by triple backquotes (```):
+    ```{keywords}```
 
-        print('Summarize clusters ...')
-        summaries = summarize_text(n_workers, [topic_model.representative_docs_[topic][0] for topic in topic_set if topic != -1], device)
-        summary_dict = dict()
-        for topic in topic_set:
-            if topic == -1:
-                summary_dict[topic] = ''
+    Create a concise label of this topic, which should not exceed 32 characters.
+    If there are more than one possible labels, return the shortest one and nothing more.
+    
+    {format_instructions}
+    """
+    
+    llm = Ollama(model="mistral:instruct")
+    output_parser = NumberedListOutputParser()
+    format_instructions = output_parser.get_format_instructions()
+
+    labeling_prompt = PromptTemplate(
+        input_variables=["documents", "keywords"], 
+        partial_variables={"format_instructions": format_instructions}, 
+        template=LABELING_PROMPT_TEMPLATE)
+    labeling_llm_chain = LLMChain(llm=llm, prompt=labeling_prompt)
+
+    # Step 1 - Extract embeddings
+    device_id = 'mps' if device == 'mps' else 0
+    # sentence_transformer = SentenceTransformer('sentence-transformers/all-mpnet-base-v2', device=device_id)
+    embedding_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2', device=device_id)
+
+    # Step 2 - Reduce dimensionality
+    umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine')
+
+    # Step 3 - Cluster reduced embeddings
+    hdbscan_model = HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
+
+    # Step 4 - Tokenize topics
+    vectorizer_model = CountVectorizer(stop_words="english")
+
+    # Step 5 - Create topic representation
+    ctfidf_model = ClassTfidfTransformer(reduce_frequent_words=True)
+
+    # Step 6 - (Optional) Fine-tune topic representations with 
+    # a `bertopic.representation` model
+    representation_model = KeyBERTInspired()
+
+    # All steps together
+    topic_model = BERTopic(
+        embedding_model=embedding_model,            # Step 1 - Extract embeddings
+        umap_model=umap_model,                      # Step 2 - Reduce dimensionality
+        hdbscan_model=hdbscan_model,                # Step 3 - Cluster reduced embeddings
+        vectorizer_model=vectorizer_model,          # Step 4 - Tokenize topics
+        ctfidf_model=ctfidf_model,                  # Step 5 - Extract topic words
+        representation_model=representation_model,  # Step 6 - (Optional) Fine-tune topic representations
+        calculate_probabilities=True,
+    )
+    
+    print('Compute embeddings ...')
+    texts = ['\n\n'.join([document[prop] for prop in ['title', 'content']]) for document in documents]
+    if device == 'cuda':
+        pool = embedding_model.start_multi_process_pool(target_devices=['cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'])
+        embeddings = embedding_model.encode_multi_process(texts, pool)
+        embedding_model.stop_multi_process_pool(pool)
+    else:
+        embeddings = embedding_model.encode(texts, show_progress_bar=True)
+
+    print('Start fit_transform ...')
+    topics, probs = topic_model.fit_transform(texts, embeddings)
+    topic_set = set(topics)
+    print(topic_model.get_topic_info())
+    
+    print('Reduce outliers ...')
+    new_topics = topic_model.reduce_outliers(texts, topics, probabilities=probs, strategy="probabilities")
+    new_topics = topic_model.reduce_outliers(texts, topics, strategy="embeddings", embeddings=embeddings)
+    topic_model.update_topics(texts, topics=new_topics)
+    print(topic_model.get_topic_info())
+    
+    n_workers = 4
+
+    print('Summarize clusters ...')
+    summaries = summarize_text(n_workers, [topic_model.representative_docs_[topic][0] for topic in topic_set if topic != -1], device)
+    summary_dict = dict()
+    for topic in topic_set:
+        if topic == -1:
+            summary_dict[topic] = ''
+        else:
+            summary_dict[topic] = summaries[topic]
+        print(f"[{topic}] --- SUM --- {summary_dict[topic]}")
+
+    print('Label clusters ...')
+    label_dict = dict()
+    for topic in topic_set:
+        if topic == -1:
+            label_dict[topic] = 'Outlier Topic'
+        else:
+            keywords = topic_model.topic_labels_[topic].split('_')[1:]
+            output = labeling_llm_chain.invoke({'documents': summary_dict[topic], 'keywords': keywords})['text']
+            result = output_parser.parse(output)
+            if result and len(result) > 0:
+                label_dict[topic] = result[0]
             else:
-                summary_dict[topic] = summaries[topic]
-            print(f"[{topic}] --- SUM --- {summary_dict[topic]}")
+                label_dict[topic] = topic_model.topic_labels_[topic]
+        print(f"[{topic}] --- LBL --- {label_dict[topic]}")
 
-        print('Label clusters ...')
-        label_dict = dict()
-        for topic in topic_set:
-            if topic == -1:
-                label_dict[topic] = 'Outlier Topic'
-            else:
-                keywords = topic_model.topic_labels_[topic].split('_')[1:]
-                output = labeling_llm_chain.invoke({'documents': summary_dict[topic], 'keywords': keywords})['text']
-                result = output_parser.parse(output)
-                if result and len(result) > 0:
-                    label_dict[topic] = result[0]
-                else:
-                    label_dict[topic] = topic_model.topic_labels_[topic]
-            print(f"[{topic}] --- LBL --- {label_dict[topic]}")
+    topic_model.set_topic_labels(label_dict)
+    print(topic_model.get_topic_info())
 
-        topic_model.set_topic_labels(label_dict)
-        print(topic_model.get_topic_info())
+    print('Classify clusters ...')
+    classified_topics_list = classify_text(n_workers, [topic_model.representative_docs_[topic][0] for topic in topic_set if topic != -1], device)
+    classified_topic_dict = dict()
+    for topic in topic_set:
+        if topic == -1:
+            classified_topic_dict[topic] = {}
+        else:
+            classified_topic_dict[topic] = classified_topics_list[topic]
+        print(f"[{topic}] --- CLS --- {classified_topic_dict[topic]}")
 
-        print('Classify clusters ...')
-        classified_topics_list = classify_text(n_workers, [topic_model.representative_docs_[topic][0] for topic in topic_set if topic != -1], device)
-        classified_topic_dict = dict()
-        for topic in topic_set:
-            if topic == -1:
-                classified_topic_dict[topic] = {}
-            else:
-                classified_topic_dict[topic] = classified_topics_list[topic]
-            print(f"[{topic}] --- CLS --- {classified_topic_dict[topic]}")
-
-        print('Answering questions ...')
-        relevant_docs_list = []
-        for topic in sorted(topic_set):
-            if topic != -1 and classified_topic_dict[topic]:
-                relevant_docs_list.append([topic, topic_model.representative_docs_[topic]])
-                
-        answers = answer_text(n_workers, [docs for _, docs in relevant_docs_list], country_dict, device)
-        answers_dict = {topic: [] for topic in topic_set}
-        for i, answer in enumerate(answers):
-            topic = relevant_docs_list[i][0]
-            answers_dict[topic] = answer
-
-        for topic in topic_set:
-            print(f"[{topic}] --- ANS --- {answers_dict[topic]}")
-
-        print('Gathering stats ...')
-        grouped_topics = {
-            topic: { 
-                'id_list': [], 'label': label_dict[topic], 'summary': summary_dict[topic], 
-                'threats': classified_topic_dict[topic], 'answers': answers_dict[topic], 
-            } for topic in topic_set
-        }
-        for index, topic in enumerate(topics):
-            grouped_topics[topic]['id_list'].append(doc_dict[pub_date][index]['id'])
+    print('Answering questions ...')
+    relevant_docs_list = []
+    for topic in sorted(topic_set):
+        if topic != -1 and classified_topic_dict[topic]:
+            relevant_docs_list.append([topic, topic_model.representative_docs_[topic]])
             
-        count = 0
-        for topic in topic_set:
-            count += len(grouped_topics[topic]['id_list'])
-        print(count, len(doc_dict[pub_date]))
-        assert count == len(doc_dict[pub_date])
-        
-        # for topic in topic_set:
-        #     if topic != -1:
-        #         text = {word: value for word, value in topic_model.get_topic(topic)}
-        #         wc = WordCloud(background_color="white", max_words=1000)
-        #         wc.generate_from_frequencies(text)
-        #         plt.imshow(wc, interpolation="bilinear")
-        #         plt.axis("off")
-        #         plt.savefig('viz/' + pub_date + '-' + str(topic) + '.png')
-        
-        cluster_file_name = 'datasets/' + pub_date + '-clusters.jsonl'
-        with open(cluster_file_name, 'wt') as out_file:
-            out_file.write(f"{json.dumps(grouped_topics)}\n")
-        print(f"\nWritten {cluster_file_name}.\n")
-        
-        print('Generating viz ...')
-        viz_hie_arch = topic_model.visualize_hierarchy(custom_labels=True)
-        viz_hie_arch.write_html("viz/" + pub_date + '-hie.html')
+    answers = answer_text(n_workers, [docs for _, docs in relevant_docs_list], country_dict, device)
+    answers_dict = {topic: [] for topic in topic_set}
+    for i, answer in enumerate(answers):
+        topic = relevant_docs_list[i][0]
+        answers_dict[topic] = answer
 
-        # Reduce dimensionality of embeddings, this step is optional but much faster to perform iteratively:
-        reduced_embeddings = UMAP(n_neighbors=10, n_components=2, min_dist=0.0, metric='cosine').fit_transform(embeddings)
-        viz_docs = topic_model.visualize_documents(texts, reduced_embeddings=reduced_embeddings, width=2048, height=1536, custom_labels=True)
-        viz_docs.write_html("viz/" + pub_date + '-cls.html')
+    for topic in topic_set:
+        print(f"[{topic}] --- ANS --- {answers_dict[topic]}")
+
+    print('Gathering stats ...')
+    grouped_topics = {
+        topic: { 
+            'id_list': [], 'label': label_dict[topic], 'summary': summary_dict[topic], 
+            'threats': classified_topic_dict[topic], 'answers': answers_dict[topic], 
+        } for topic in topic_set
+    }
+    for index, topic in enumerate(topics):
+        grouped_topics[topic]['id_list'].append(documents[index]['id'])
         
-        end_time = datetime.now()
-        seconds = (end_time - start_time).total_seconds()
-        print(f"\nTotal {len(doc_dict[pub_date])} documents in {seconds} seconds: {seconds*1000/(len(doc_dict[pub_date])):0.3f} seconds per 1K documents.", flush=True)
+    count = 0
+    for topic in topic_set:
+        count += len(grouped_topics[topic]['id_list'])
+    print(count, len(documents))
+    assert count == len(documents)
+    
+    # for topic in topic_set:
+    #     if topic != -1:
+    #         text = {word: value for word, value in topic_model.get_topic(topic)}
+    #         wc = WordCloud(background_color="white", max_words=1000)
+    #         wc.generate_from_frequencies(text)
+    #         plt.imshow(wc, interpolation="bilinear")
+    #         plt.axis("off")
+    #         plt.savefig('viz/' + pub_date + '-' + str(topic) + '.png')
+    
+    if end_date is None:
+        cluster_file_name = 'datasets/' + single_date + '-clusters.jsonl'
+    else:
+        cluster_file_name = 'datasets/' + single_date + '-' + end_date + '-clusters.jsonl'
+    with open(cluster_file_name, 'wt') as out_file:
+        out_file.write(f"{json.dumps(grouped_topics)}\n")
+    print(f"\nWritten {cluster_file_name}.\n")
+    
+    print('Generating viz ...')
+    if end_date is None:
+        viz_file_name = "viz/" + single_date + '-hie.html'
+    else:
+        viz_file_name = "viz/" + single_date + '-' + end_date + '-hie.html'
+    viz_hie_arch = topic_model.visualize_hierarchy(custom_labels=True)
+    viz_hie_arch.write_html(viz_file_name)
+
+    if end_date is None:
+        viz_file_name = "viz/" + single_date + '-cls.html'
+    else:
+        viz_file_name = "viz/" + single_date + '-' + end_date + '-cls.html'
+    # Reduce dimensionality of embeddings, this step is optional but much faster to perform iteratively:
+    reduced_embeddings = UMAP(n_neighbors=10, n_components=2, min_dist=0.0, metric='cosine').fit_transform(embeddings)
+    viz_docs = topic_model.visualize_documents(texts, reduced_embeddings=reduced_embeddings, width=2048, height=1536, custom_labels=True)
+    viz_docs.write_html(viz_file_name)
+    
+    end_time = datetime.now()
+    seconds = (end_time - start_time).total_seconds()
+    print(f"\nTotal {len(documents)} documents in {seconds} seconds: {seconds*1000/(len(documents)):0.3f} seconds per 1K documents.", flush=True)
 
---
+++ backend/scripts/cluster-period-news-articles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+device=$1
+
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2019-12-31 $device 2020-01-02
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-01 $device 2020-01-03
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-02 $device 2020-01-04
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-03 $device 2020-01-05
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-01-04 $device 2020-01-06
+
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2019-12-31 $device 2020-01-06
+
+python pipeline/cluster-news-articles.py datasets datasets/country-codes.tsv 2020-12-31 $device 2020-01-29
